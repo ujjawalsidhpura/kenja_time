0ee7ec92c35dd52f0a22271b8eee2e3947583955
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-17 13:36:09 -0500

    add 3d model

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,7 +1,9 @@
+import Model3D from "./Model3D"
+
 export default function Sidebar() {
     return (
         <div className="flex-1 bg-stone-50">
-
+            <Model3D />
         </div>
     )
 }
