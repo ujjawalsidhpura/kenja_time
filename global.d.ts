147f8b9d615fa4b214358ecb880e0e17b34a4159
ujjawal sidhpura <ujjawalsidhpura@gmail.com>
2022-02-19 11:36:17 -0500

    flowbite

---
+++ global.d.ts
@@ -0,0 +1,5 @@
+declare module '@material-tailwind/react/Button';
+declare module '@material-tailwind/react/ModalFooter';
+declare module '@material-tailwind/react/ModalBody';
+declare module '@material-tailwind/react/ModalHeader';
+declare module '@material-tailwind/react/Modal';
