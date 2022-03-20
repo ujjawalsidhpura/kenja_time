b54b5e84fd406c84477dcfbf97796b0cbaefed29
nojuku <vluchnikov@yahoo.ca>
2022-03-20 11:29:32 -0400

    testing GIF compatability

--- src/Data/Projects/ProjectsData.js
+++ src/Data/Projects/ProjectsData.js
@@ -1,6 +1,6 @@
 /* Import each project here */
 import passersby1 from "./passersby.jpeg";
-import passersby2 from "./passersby2.jpeg";
+import foldable_universe from "./foldable_universe.gif";
 
 export default function ProjectsData() {
 
@@ -8,16 +8,15 @@ export default function ProjectsData() {
 
         project1: {
             picture: passersby1,
-            title: 'TitleOne',
-            tags: '#tag1',
+            title: 'Passers-by',
+            tags: '#Rpi', "#Physical_computing",
             description: '“Passers By” is an interactive public installation allowing passers-by to connect with cold,unwelcoming spaces, temporarily emerge from the unstoppable stream of thought and feel comfort."Encouraging observation" is the motivation behind the project as we believe that lack of curiosity, in part, increases levels of stress and loneliness in the city dwellers. Passers By is an attempt to use technology not as an informational (spoken word) weapon but rather as a tool to generate emotion, empathy. As a secondary task we hope that the piece can contribute to establishing non-verbal dialogue where space can become the connecting intermediary between strangers.'
         },
         project2: {
-            picture: passersby2,
-            title: 'TitleTwo',
-            tags: '#TagTwo',
-            description: '“Passers By” is an interactive public installation allowing passers-by to connect with cold,unwelcoming spaces, temporarily emerge from the unstoppable stream of thought and feel comfort."Encouraging observation" is the motivation behind the project as we believe that lack of curiosity, in part, increases levels of stress and loneliness in the city dwellers. Passers By is an attempt to use technology not as an informational (spoken word) weapon but rather as a tool to generate emotion, empathy. As a secondary task we hope that the piece can contribute to establishing non-verbal dialogue where space can become the connecting intermediary between strangers.'
-
+            picture: foldable_universe,
+            title: 'Foldable Universe',
+            tags: '#MaxMsp',
+            description: 'static self-generating visual art piece'
         },
         /* Extra projects go here */
     }
