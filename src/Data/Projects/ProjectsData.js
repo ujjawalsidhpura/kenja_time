7ab40917ba65d08ce67efd02d552dbe94c42f543
Ujjawal Sidhpura <85900088+ujjawalsidhpura@users.noreply.github.com>
2022-03-20 20:07:56 -0400

    Update ProjectsData.js
    Edit projefcts data error

--- src/Data/Projects/ProjectsData.js
+++ src/Data/Projects/ProjectsData.js
@@ -9,7 +9,7 @@ export default function ProjectsData() {
         project1: {
             picture: passersby1,
             title: 'Passers-by',
-            tags: '#Rpi', "#Physical_computing",
+            tags: "#Rpi, #Physical_computing",
             description: '“Passers By” is an interactive public installation allowing passers-by to connect with cold,unwelcoming spaces, temporarily emerge from the unstoppable stream of thought and feel comfort."Encouraging observation" is the motivation behind the project as we believe that lack of curiosity, in part, increases levels of stress and loneliness in the city dwellers. Passers By is an attempt to use technology not as an informational (spoken word) weapon but rather as a tool to generate emotion, empathy. As a secondary task we hope that the piece can contribute to establishing non-verbal dialogue where space can become the connecting intermediary between strangers.'
         },
         project2: {
